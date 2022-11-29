8b064a686b04448f650f2dd61813d51fbfbd352e
ichirugame <101532725+ichirugame@users.noreply.github.com>
2022-11-29 21:19:39 +0900

    初期

--- index.php
+++ index.php
@@ -5,14 +5,14 @@ $pwd = "";
 $data_base = "";
 $query = file_get_contents("./sql/db.sql");
 $link = mysqli_connect($db_host, $user, $pwd, $data_base);
-$db_sl = mysqli_real_query($link, $query);
+$db_in = mysqli_multi_query($link, $query);
 ?>
 <html>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <title>インポート</title>
-<?php if($db_sl){ ?>
+<?php if($db_in){ ?>
 <p>インポートに成功しました。</p>
 <?php }else{ ?>
 <p>インポートに失敗しました。</p>
