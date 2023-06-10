cf9c34e535065bfe32bc8c48ac5bc13bb67768fa
ichirugame <101532725+ichirugame@users.noreply.github.com>
2023-06-11 00:21:02 +0900

    変更

--- object.php
+++ object.php
@@ -12,15 +12,19 @@ $query = file_get_contents("./sql/db.sql");
 $mysqli = new mysqli($db_host, $user, $pwd, $data_base);
 $db_in = $mysqli -> multi_query($query);
 ?>
-<html>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<title>インポート</title>
-<?php if($db_in){ ?>
-<p>インポートに成功しました。</p>
-<?php }else{ ?>
-<p>インポートに失敗しました。</p>
-<?php } ?>
-<?php $mysqli->close(); ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>インポート</title>
+</head>
+<body>
+    <?php if($db_in){ ?>
+        <p>インポートに成功しました。</p>
+    <?php }else{ ?>
+        <p>インポートに失敗しました。</p>
+    <?php } ?>
+    <?php $mysqli->close(); ?>
+</body>
 </html>
--- procedure.php
+++ procedure.php
@@ -13,15 +13,19 @@ $query = file_get_contents("./sql/db.sql");
 $link = mysqli_connect($db_host, $user, $pwd, $data_base);
 $db_in = mysqli_multi_query($link, $query);
 ?>
-<html>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width,initial-scale=1">
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<title>インポート</title>
-<?php if($db_in){ ?>
-<p>インポートに成功しました。</p>
-<?php }else{ ?>
-<p>インポートに失敗しました。</p>
-<?php } ?>
-<?php mysqli_close($link); ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>インポート</title>
+</head>
+<body>
+    <?php if($db_in){ ?>
+        <p>インポートに成功しました。</p>
+    <?php }else{ ?>
+        <p>インポートに失敗しました。</p>
+    <?php } ?>
+    <?php mysqli_close($link); ?>
+</body>
 </html>
